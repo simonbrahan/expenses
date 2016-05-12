51cbd7e83a26792549b2e1a6ead487a6b0d0e954
simon brahan <tntpiranha@gmail.com>
2016-05-12 22:05:53 +0100

    1

--- wsgi.py
+++ wsgi.py
@@ -13,7 +13,7 @@ import jinja2
 def application(environ, start_response):
     ctype = 'text/html'
 
-    response_body = ['%s: %s' % (key, value) for key, value in sorted(environ.items())]
+    response_body = 'kebbles' # ['%s: %s' % (key, value) for key, value in sorted(environ.items())]
 
     status = '200 OK'
     response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
