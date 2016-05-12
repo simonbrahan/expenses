0b74777843d8f06de429e359d652f25ca61d6b09
simon brahan <tntpiranha@gmail.com>
2016-05-12 22:59:38 +0100

    1

--- wsgi.py
+++ wsgi.py
@@ -21,8 +21,6 @@ def application(environ, start_response):
     template = template_env.get_template('index.htm')
     response_body = template.render({ 'grouped_expenses': grouped_expenses })
 
-    response_body = environ['DOCUMENT_ROOT'] + 'templates'
-
     response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(response_body)))]
 
     start_response('200 OK', response_headers)
