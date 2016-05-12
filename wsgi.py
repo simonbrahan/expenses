b5ee89f059925b58f781172e9410422aee050450
simon brahan <tntpiranha@gmail.com>
2016-05-12 22:46:50 +0100

    1

--- wsgi.py
+++ wsgi.py
@@ -21,7 +21,7 @@ def application(environ, start_response):
     template = template_env.get_template('index.htm')
     response_body = template.render({ 'grouped_expenses': grouped_expenses })
 
-    response_body = 'kebbles'
+    #response_body = 'kebbles'
 
     response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(response_body)))]
 
