f2dc985b44a3c80221a22e6e185ad2f553f0c207
simon brahan <tntpiranha@gmail.com>
2016-05-12 22:06:57 +0100

    1

--- wsgi.py
+++ wsgi.py
@@ -11,12 +11,10 @@ except IOError:
 import jinja2
 
 def application(environ, start_response):
-    ctype = 'text/html'
 
-    response_body = 'kebbles' # ['%s: %s' % (key, value) for key, value in sorted(environ.items())]
+    response_body = ['%s: %s' % (key, value) for key, value in environ.items()]
 
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(response_body)))]
 
-    start_response(status, response_headers)
+    start_response('200 OK', response_headers)
     return [response_body]
