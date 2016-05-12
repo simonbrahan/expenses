94e5830fe9116e49cf0f0eda6cd0b820c73e3f3f
simon brahan <tntpiranha@gmail.com>
2016-05-12 21:45:43 +0100

    1

--- wsgi.py
+++ wsgi.py
@@ -8,21 +8,18 @@ try:
 except IOError:
     pass
 
-def application(environ, start_response):
+import cgitb
+from lib.expenses import expense
+from cgi import FieldStorage
+import jinja2
 
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                    for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
-    else:
-        ctype = 'text/html'
-        response_body = 'Hi'
+
+def application(environ, start_response):
+    ctype = 'text/html'
+    response_body = type(environ)
 
     status = '200 OK'
     response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
+
     start_response(status, response_headers)
     return [response_body]
